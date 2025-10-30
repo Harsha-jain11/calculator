30bd408c92d9b8bdd54052730de878aabffb723d
Harshita.Jujar <01fe24ica051@kletech.ac.in>
2025-10-30 20:15:16 +0530

    Modified with multiplication and division functionalities

--- basiccalculator.py
+++ basiccalculator.py
@@ -5,7 +5,14 @@ num2 = float(input("Enter the second number: "))
 # Perform addition and subtraction
 addition = num1 + num2
 subtraction = num1 - num2
+multiplication = num1 * num2
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "Undefined (cannot divide by zero)"
 # Display the results
 print("\nResults:")
 print("Addition:", addition)
 print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
